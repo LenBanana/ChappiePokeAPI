c7a7fcded046c7a58cae57b255ff73bfe1749606
len <lenstandvoss@yahoo.de>
2020-12-02 00:11:59 +0100

    edited asset path

--- ChappiePokeAPI/Controllers/MainController.cs
+++ ChappiePokeAPI/Controllers/MainController.cs
@@ -35,10 +35,11 @@ namespace ChappiePokeAPI.Controllers
                     //var fileType = Path.GetExtension(file.FileName);
                     //if (fileType.ToLower() == ".pdf" || fileType.ToLower() == ".jpg" || fileType.ToLower() == ".png" || fileType.ToLower() == ".jpeg")
                     //{
-                    var docName = Path.GetFileName(file.FileName);
+                    var docExt = System.IO.Path.GetExtension(file.FileName).ToString();
+                    var fileName = Path.GetRandomFileName() + docExt;
                     if (file != null && file.Length > 0)
                     {
-                        string filePath = Path.Combine(Paths.AssetUploadPath, file.FileName);
+                        string filePath = Path.Combine(Paths.AssetUploadPath, fileName);
                         using (var stream = new FileStream(filePath, FileMode.Create))
                         {
                             await file.CopyToAsync(stream);
